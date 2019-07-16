1492fe4162eca8924ad4a43ddc19dd080df4118b
vagrant <vagrant@homestead>
2019-07-16 14:10:03 +0000

    MsgType

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -12,8 +12,28 @@ class WechatController extends Controller
 //        log::info('request arrived.');
 
         $wechat = app('wechat');
-        $wechat->server->setMessageHandler(function ($message){
-            return "欢迎关注 Spring";
+        $userApi = $wechat->user;
+        $wechat->server->setMessageHandler(function ($message) use ($userApi){
+            switch ($message->MsgType){
+                case 'event':
+                    # 事件消息.....
+                    break;
+                case 'text':
+                    # 文字消息····
+                    return '你好 ！' . $userApi->get($message->FromUserName)->nickname;
+                    break;
+                case 'image':
+                    # 图片消息····
+                    break;
+                case 'voice':
+                    # 语言消息·····
+                    break;
+                    //其他消息····
+                default:
+                    #Code
+                    break;
+
+            }
         });
 
 //        log::info('return response.');
